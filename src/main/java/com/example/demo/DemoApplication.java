3c88d3f6dcbd50d3f65965271aa49bae52fe31e8
lucas <deliberato.lucas@gmail.com>
2022-05-29 19:33:51 -0300

    feat cccccc

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.println("aaaaaa");
 		System.println("bbbbbb");
+		System.println("ccccccc");
 		System.out.println("msg melhorada");
 	}
 
