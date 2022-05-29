4420bbba91d5a1e71a2aa744e71773274b63fe7f
Lucas Deliberato <deliberato.lucas@gmail.com>
2022-05-29 15:06:14 -0300

    Merge pull request #1
    * msg a
    
    * msg a fix
    
    * msg bbbb
    
    * Merge branch 'master' into feat2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.println("aaaaaa");
+		System.println("bbbbbb");
 		System.out.println("msg melhorada");
 	}
 
